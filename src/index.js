217e9a876e6b8f0488e63dc3e3742100f16f951e
thuongb14 <thuongb14@gmail.com>
2022-09-15 23:27:27 +1000

    Add renderTask

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './style.css';
 
-
-//in DOM.js
 const addProject = document.querySelector('.add-project-button');
 const projectForm = document.querySelector("#project-form");
 const cancelProjectBtn = document.querySelector('.project-cancel-btn');
 const projectList = document.querySelector('.project-list');
+const heading = document.querySelector('#heading')
+const buttonProject = document.querySelectorAll('.button-project')
+const taskList = document.querySelector('#task-list')
 
 //in UI.js
 class UI {
@@ -24,9 +25,13 @@ class UI {
         if(item.title === '') {
             alert('Please fill in project name')
         } else {
-            projectList.insertAdjacentHTML('beforeend' , `<button class="button-project"><i class="material-icons">task</i>${item.title}<i class="material-icons" style="margin-left: auto">cancel</i></button>
-            `)
+            item.id = Date.now().toString()
+            projectList.insertAdjacentHTML('beforeend' , 
+            `<button class="button-project" id=${item.id}>
+            <i class="fa-solid fa-tasks"></i>${item.title}
+            <i class="fa-solid fa-xmark delete-project" style="color:red; float: right"></i></button>`)
             ui.removeProjectForm()
+            myProjects.push(item)
         }
     }
 
@@ -34,47 +39,79 @@ class UI {
         document.getElementById('project-name').value = '';
     }
 
-    deleteProject() {
-        
+    deleteProject(e) {
+        if(e.target.classList.contains('delete-project')) {
+            e.target.parentElement.remove()
+            heading.innerHTML = '';
+            taskList.innerHTML = ''
+        }
     }
+
+    renderChosenProject(e) {
+        if(buttonProject) {
+            let selected = myProjects.find(item => item.id === e.target.id)
+            heading.innerText = selected.title;
+            ui.renderTask(e)
+        }
+    }
+
+    renderTask(e) {
+        if(buttonProject) {
+            let selected = myProjects.find(item => item.id === e.target.id)
+            console.log(selected, selected.task)
+            selected.task.forEach((task) => {
+                let li = document.createElement('li');
+                li.textContent = task;
+                taskList.appendChild(li)
+            })
+        }
+
+        //get the id of chosen list
+        //get the project of that id
+        //push the task into the inner HTMl of div content
+        }
+
 }
 
 //in project.js
 class Projects {
-    constructor(title) {
+    constructor(title, id) {
         this.title = title;
+        this.task = ['Hello', 'world'];
+        this.id = id;
     }
-
-    deleteProjectList() {
-        ui.deleteProject()
-    }
+  
+    deleteProjectList(e) {
+    const selected = e.target.parentElement.id;
+    myProjects = myProjects.filter(item => item.id !== selected)
+    ui.deleteProject(e)
+  }
 }
 
 let myProjects = []
-
 
 const ui = new UI();
 
 //in DOM.js
 const DOM = (() => {
-
+    document.addEventListener('click', function(e) {
+        const project = new Projects()
+        project.deleteProjectList(e)
+        ui.renderChosenProject(e)
+    })
     projectForm.addEventListener('submit', function(e) {
-        const project = new Projects(title)
+        const project = new Projects()
         e.preventDefault();
         ui.addProject(project)
-        myProjects.push(project)
-        console.log(myProjects)
     })
 
     addProject.addEventListener('click', ui.showProjectForm);
 
     cancelProjectBtn.addEventListener('click', ui.removeProjectForm)
+
 })();
-    
 
 
-
- 
 
 
 
